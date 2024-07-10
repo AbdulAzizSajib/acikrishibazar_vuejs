9d82fe1e43a20ef83a92ebecb7718276bb3dde2b
Abdul Aziz Sajib <abdulazizsajib@gmail.com>
2024-07-10 20:13:38 +0600

    Navbar Updated

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -11,12 +11,18 @@ import {
   HeartIcon,
   ShoppingCartIcon,
   UserIcon,
+  MagnifyingGlassIcon,
 } from "@heroicons/vue/24/outline";
 </script>
 
 <template>
-  <div class="navbar bg-base-200">
-    <div class="navbar-start border">
+  <div
+    class="navbar space-x-2 px-4 mx-auto bg-base-200 max-w-full flex flex-wrap"
+  >
+    <!-- Navbar Start -->
+    <div
+      class="flex justify-between w-full md:w-auto md:justify-start md:space-x-4"
+    >
       <div class="dropdown">
         <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
           <svg
@@ -43,33 +49,75 @@ import {
           </li>
         </ul>
       </div>
-      <a class="text-green-700 text-3xl font-bold">ACI Krishibazar</a>
+      <a class="text-green-700  md:text-[2vw] font-semibold">
+        ACI Krishibazar
+      </a>
+
+      <!-- Help & Contact and Order Tracking -->
+      <div class="hidden lg:flex gap-4">
+        <div class="flex items-center space-x-2">
+          <img
+            class="w-6 h-6"
+            src="/src/assets/images/icons/chat.png"
+            alt="Live Chat"
+          />
+          <a href="#" class="hidden lg:inline-block text-md text-green-700">
+            Help & Contact
+          </a>
+        </div>
+        <div class="flex items-center space-x-2">
+          <img
+            class="w-6 h-6"
+            src="/src/assets/images/icons/truck.png"
+            alt="Order Tracking"
+          />
+          <a href="#" class="hidden lg:inline-block text-md text-green-700">
+            Order Tracking
+          </a>
+        </div>
+      </div>
     </div>
-    <!-- <div class="navbar-center hidden border lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li v-for="link in navbarLinks" :key="link.name">
-          <router-link :to="link.link">{{ link.name }}</router-link>
-        </li>
-      </ul>
-    </div> -->
-    <div class="flex-1 border">
-      <input type="search" name="" id="" />
-    </div>
-    <div class="border space-x-1">
-      <button
-        class="p-2 rounded-full group hover:bg-white hover:border-2 hover:border-green-600 bg-green-600"
+
+    <!-- Search Bar -->
+    <label
+      class="input input-bordered flex rounded-full w-full md:w-[50%] items-center gap-2 mx-2 mt-2 md:mt-0"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        class="h-6 w-6 opacity-70"
       >
-        <BellIcon class="size-6 text-white group-hover:text-green-600" />
+        <path
+          fill-rule="evenodd"
+          d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <input type="text" class="grow" placeholder="Search" />
+    </label>
+
+    <!-- Icons -->
+    <div
+      class="border  flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-1 lg:space-x-2 mt-2 w-full md:w-auto md:mt-0"
+    >
+    <div class="space-x-1 flex ">
+      <button
+        class="p-2  rounded-full group hover:bg-white hover:border-2 hover:border-green-600 bg-green-600"
+      >
+        <BellIcon class="w-6 h-6 text-white group-hover:text-green-600" />
       </button>
       <button class="p-2 rounded-full bg-green-600">
-        <HeartIcon class="size-6 text-white" />
+        <HeartIcon class="w-6 h-6 text-white" />
       </button>
       <button class="p-2 rounded-full bg-green-600">
-        <ShoppingCartIcon class="size-6 text-white" />
+        <ShoppingCartIcon class="w-6 h-6 text-white" />
       </button>
       <button class="p-2 rounded-full bg-green-600">
-        <UserIcon class="size-6 text-white" />
+        <UserIcon class="w-6 h-6 text-white" />
       </button>
+    </div>
+     
     </div>
   </div>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,78 @@
 <template>
- <Mainlayout>
-  <div class="text-5xl">Login Page</div>
- </Mainlayout>
+  <Mainlayout>
+    <div class="hero min-h-screen">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <div
+          class="card shadow-md shadow-indigo-200 border w-full md:w-[512px] md:h-[371px] max-w-lg shrink-0"
+        >
+          <h2 class="text-center text-3xl font-bold mt-4 uppercase">Sign in</h2>
+          <form @submit.prevent="handleLogin($router)" class="card-body">
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                class="input input-bordered"
+                v-model="formData.email"
+                required
+              />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                class="input input-bordered"
+                v-model="formData.pass"
+                required
+              />
+              <label class="label">
+                <a href="#" class="label-text-alt link link-hover"
+                  >Forgot password?</a
+                >
+              </label>
+            </div>
+            <div class="form-control">
+              <button
+                type="submit"
+                class="btn bg-green-500 rounded-full text-white text-lg text-bold"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </Mainlayout>
 </template>
 
 <script setup>
-
-import Mainlayout from "../components/MainLayOut/Mainlayout.vue"
-
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import { reactive } from "vue";
+import Mainlayout from "../components/MainLayOut/Mainlayout.vue";
+import Cookies from "js-cookie";
+// submit Button
+const formData = reactive({
+  email: null,
+  password: null,
+});
+const handleLogin = (router) => {
+  if (formData.email != "aci@com") {
+    toast.error("Email is not correct");
+    return 0;
+  } else {
+    Cookies.set("token", "Token123456");
+    toast.success("You've logged in", "success");
+    setTimeout(() => router.push({ name: "home" }), 2000);
+  }
+  console.log("submit", formData);
+};
 </script>
 
 <style lang="scss" scoped></style>
